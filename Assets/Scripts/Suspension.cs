0836d1f86a21f1cbd9bf57f201952219b1a476da
hayden-donnelly <hayden.git@proton.me>
2019-04-03 19:43:36 -0400

    Init

---
+++ Assets/Scripts/Suspension.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Suspension : MonoBehaviour 
+{
+	[SerializeField] private Rigidbody vehicleBody;
+	[SerializeField] private float wheelRadius = 0.5f;
+	[SerializeField] private float suspensionDistanceConstant = 1f;
+	[SerializeField] private float springConstant = 30000f;
+	[SerializeField] private float damperConstant = 4000f;
+
+	private float previousSuspensionDistance;
+	private float currentSuspensionDistance;
+	private float springVelocity;
+	private float springForce;
+	private float damperForce;
+
+	private void FixedUpdate()
+	{
+		// Vehicle uses a raycast suspension system
+		RaycastHit hit;
+		Ray ray = new Ray(transform.position, -transform.up);
+		if(Physics.Raycast(ray, out hit, suspensionDistanceConstant + wheelRadius))
+		{
+			// Hooke's Law
+			previousSuspensionDistance = currentSuspensionDistance;
+			currentSuspensionDistance = suspensionDistanceConstant - (hit.distance - wheelRadius);
+			springVelocity = (currentSuspensionDistance - previousSuspensionDistance) / Time.fixedDeltaTime;
+			springForce = springConstant * currentSuspensionDistance;
+			damperForce = damperConstant * springVelocity;
+
+			// Apply force to car body
+			vehicleBody.AddForceAtPosition(transform.up * (springForce + damperForce), transform.position, ForceMode.Force);
+		}
+
+		Debug.DrawRay(transform.position, -transform.up * (suspensionDistanceConstant), Color.blue, 0f);
+	}
+}
---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour 
+{
+	[Header("Rigidbody")]
+	[SerializeField] private Vector3 centerOfMass;
+	[Header("Acceleration")]
+	[SerializeField] private float accelSpeed;
+	[SerializeField] private ForceMode accelMode;
+	[SerializeField] private Vector3 accelOrigin;
+	[Header("Turning")]
+	[SerializeField] private float turnSpeed;
+	[SerializeField] private ForceMode turnMode;
+	[Header("Traction")]
+	[SerializeField] private float normalTractionConstant;
+	[SerializeField] private float driftTractionConstant;
+	private float tractionConstant;
+	[SerializeField] private ForceMode tractionMode;
+	[Header("Debug")]
+	[SerializeField] private float velocityDebugRayLength;
+	[SerializeField] private float currentSpeed;
+
+	private Rigidbody rb;
+	private float axisH;
+	private float axisV;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.centerOfMass = centerOfMass;
+		tractionConstant = normalTractionConstant;
+	} 
+
+	private void Update()
+	{
+		// Get Input
+		axisH = Input.GetAxis("Horizontal");
+		axisV = Input.GetAxis("Vertical");
+	}
+
+	private void FixedUpdate()
+	{
+		currentSpeed = rb.velocity.magnitude;
+
+		// Acceleration
+		Vector3 desiredAccel = transform.forward * accelSpeed * axisV * Time.fixedDeltaTime;
+		rb.AddForceAtPosition(desiredAccel, transform.position + accelOrigin, accelMode);
+
+		// Turn
+		if(axisV != 0)
+		{
+			Vector3 desiredTurn = Vector3.up * turnSpeed * axisH * Time.fixedDeltaTime;
+			rb.AddTorque(desiredTurn, turnMode);
+		}
+
+		// Traction
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			tractionConstant = driftTractionConstant;
+		}
+		if(Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			tractionConstant = normalTractionConstant;
+		}
+		Vector3 desiredTraction = transform.InverseTransformDirection(rb.velocity);
+		rb.AddForce(transform.right * -desiredTraction.x * tractionConstant);
+	}
+}
